ec0ab7266c002826e648ae7b59ae8759d2d89e2f
sean-lai-sh <seanlai221@gmail.com>
2022-05-05 19:03:06 +0800

    gitignore setup

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -54,22 +54,30 @@ const Work = () => {
           <a href={work.projectLink} target="_blank" rel="noreferrer">
             <motion.div
             whileHover={{scale:[1,0.9]}}
-            whileInView={{scale:[0,1]}}
-          transition={{duration:0.25}}
-          className='app__flex'>
+              whileInView={{scale:[0,1]}}
+              transition={{duration:0.25}}
+              className='app__flex'>
             <AiFillEye/>
           </motion.div>
           </a>
-          <a href={work.projectLink} target="_blank" rel="noreferrer">
-            <motion.div
-            whileHover={{scale:[1,0.9]}}
-            whileInView={{scale:[0,1]}}
-          transition={{duration:0.25}}
-          className='app__flex'>
-            <AiFillGithub/>
+            <a href={work.projectLink} target="_blank" rel="noreferrer">
+              <motion.div
+                  whileHover={{scale:[1,0.9]}}
+                  whileInView={{scale:[0,1]}}
+                  transition={{duration:0.25}}
+                  className='app__flex'>
+                <AiFillGithub/>
+              </motion.div>
+            </a>
           </motion.div>
-          </a>
-          </motion.div>
+          <div className='app__work-content app__flex'>
+            <h4 className='bold-text'>{work.title}</h4>
+            <p className='p-text' style={{marginTop:10}}>{work.description}</p>
+            <div className='app__work-tag app__flex'>
+              <p className='p-text'>{work.tags[0]}</p>
+
+            </div>
+          </div>
         </div>))}
 
       </motion.div>
